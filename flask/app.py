6a73f7ee81e44a5fd0b468676039fb8ae328a069
js9537 <js9537@g.rit.edu>
2023-07-12 20:28:59 -0700

    commit

--- flask/app.py
+++ flask/app.py
@@ -16,18 +16,19 @@ current_year = datetime.datetime.now().year
 
 # Get the current working directory
 current_path = os.getcwd()
-with open(current_path+'/extravagant_koalas/flask/pkls/tfidf_vectorizer.pkl', 'rb') as file:
+print(current_path)
+with open(current_path+'/pkls/tfidf_vectorizer.pkl', 'rb') as file:
         tfidf_vectorizer = pickle.load(file)
 
 
-with open(current_path+'/extravagant_koalas/flask/pkls/kmeans.pkl', 'rb') as file:
+with open(current_path+'/pkls/kmeans.pkl', 'rb') as file:
          kmeans =  pickle.load(file)
          
-with open(current_path+'/extravagant_koalas/flask/pkls/documents.pkl', 'rb') as file:
+with open(current_path+'/pkls/documents.pkl', 'rb') as file:
          documents =  pickle.load(file)
 documents = documents.reset_index().rename(columns={'level_0': 'documentNum'})
 
-with open(current_path+'/extravagant_koalas/flask/pkls/documents_idf.pkl', 'rb') as file:
+with open(current_path+'/pkls/documents_idf.pkl', 'rb') as file:
          tfidf_df =  pickle.load(file)
 
 
